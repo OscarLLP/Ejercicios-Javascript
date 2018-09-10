52ccff31953e88c0d1d6779f1072a17897202d64
oscar <oscar.lopez@imaginamos.com.co>
2018-09-10 09:38:17 -0500

    ejercicios_javscript

--- javascript/javascript-1.js
+++ javascript/javascript-1.js
@@ -1,1 +1,9 @@
-alert("hello wordl");
+let arra = [1,2,3,4,'oscar',5,6,7,8,9,];
+let suma = 0;
+for (i = 0; i<arra.length; i++){
+  let element = arra[i];
+  if (typeof element == 'number'){
+    suma += element;
+  }
+}
+ console.log(suma);
